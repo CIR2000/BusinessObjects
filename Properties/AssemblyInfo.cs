e3fdd442623590fdaf8358a71d042ec969897525
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-03-17 10:13:27 +0100

    AssemblyInfo updates

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BusinessObjects")]
+[assembly: AssemblyDescription("Minimalist object-oriented business layer for your app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CIR2000")]
+[assembly: AssemblyProduct("BusinessObjects.Properties")]
+[assembly: AssemblyCopyright("CIR2000")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ac9083e-a307-4073-bed5-f5059675b7f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
