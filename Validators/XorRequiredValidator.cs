0e9f63d9256603b3db61262bb2e44e09d467d570
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-03-23 11:35:46 +0100

    XorRequiredValdiator

---
+++ Validators/XorRequiredValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace BusinessObjects.Validators
+{
+    public class XorRequiredValidator : Validator {
+
+        public XorRequiredValidator(string[] properties) : base(properties, "Required."){ }
+        public XorRequiredValidator(string[] properties, string description) : base(properties, description){ }
+
+        /// <summary>
+        /// Validates that the rule has been followed.
+        /// </summary>
+        public override bool Validate(BusinessObject businessObject) {
+            var cnt = Properties.Select(prop => GetPropertyValue(businessObject, prop)).Count(v => v != null);
+            return cnt == 1;
+        }
+    }
+}
